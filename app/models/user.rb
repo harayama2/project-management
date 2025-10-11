e8219c7ac8a91eacbd20d402ec774a12cd32f164
harayama2 <44060633+harayama2@users.noreply.github.com>
2025-10-11 23:26:05 +0900

    Remove devise registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable
 
   enum :role, { member: 0, admin: 1 }
 end
